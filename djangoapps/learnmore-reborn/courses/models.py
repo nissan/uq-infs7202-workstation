ce61111f5429f6715825aa36b9219057fcf2686b
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-05-19 14:15:12 +1000

    Initial commit of learnmore-reborn

--- djangoapps/learnmore-reborn/courses/models.py
+++ djangoapps/learnmore-reborn/courses/models.py
@@ -11,3 +11,21 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Module(models.Model):
+    course = models.ForeignKey('Course', related_name='modules', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    order = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.course.title} - {self.title}"
+
+class Quiz(models.Model):
+    module = models.ForeignKey('Module', related_name='quizzes', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    is_survey = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.module.title} - {self.title}"
--- djangoapps/learnmore-reborn/courses/views.py
+++ djangoapps/learnmore-reborn/courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Module, Quiz
 
-# Create your views here.
+class ModuleDetailView(DetailView):
+    model = Module
+    template_name = 'courses/module_detail.html'
+
+class QuizDetailView(DetailView):
+    model = Quiz
+    template_name = 'courses/quiz_detail.html'
