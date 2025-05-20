75b844d7534745944de780e4a6b26237c4cb1f80
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-05-20 11:04:24 +1000

    add user models and integrate

--- djangoapps/learnmore-reborn/users/admin.py
+++ djangoapps/learnmore-reborn/users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_is_instructor')
+    list_select_related = ('profile',)
+
+    def get_is_instructor(self, instance):
+        return instance.profile.is_instructor
+    get_is_instructor.short_description = 'Instructor'
+    get_is_instructor.boolean = True
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(UserAdmin, self).get_inline_instances(request, obj)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
